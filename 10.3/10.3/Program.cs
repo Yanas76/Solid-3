4af7300ecf833ab8f3252e051e7d39775cb50e89
Yanas76 <116164567+Yanas76@users.noreply.github.com>
2022-12-02 00:14:53 +0200

    Add files via upload

---
+++ 10.3/10.3/Program.cs
@@ -0,0 +1,36 @@
+﻿//До помилкового результату призвело порушення принципу підстановки Лісков
+
+class Rectangle
+{
+    public virtual int Width { get; set; }
+    public virtual int Height { get; set; }
+    public int GetRectangleArea()
+    {
+        return Width * Height;
+    }
+}
+
+//квадрат наслідується від прямокутника!!!
+class Square : Rectangle
+{
+    public int Side
+    {
+        get { return Width; }
+        set
+        {
+            base.Height = value;
+            base.Width = value;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Width = 5;
+            rect.Height = 10;
+            Console.WriteLine(rect.GetRectangleArea());
+            Console.ReadKey();
+        }
+    }
+}
